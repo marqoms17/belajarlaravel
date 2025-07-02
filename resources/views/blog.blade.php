8ffcbbc2982b78da5f66e33da35611b07f784bc1
Marqoms <marqoms17@gmail.com>
2025-07-02 22:24:44 +0700

    add blog post

--- resources/views/blog.blade.php
+++
@@ -1,4 +0,0 @@
-<x-layout :title="$title">
-    <!-- Your content -->
-    <p>Welcome to my {{ $title }}</p>
-</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,28 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home', ['title' => 'Home Page']);
 });
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog']);
+Route::get('/posts', function () {
+    $posts = [
+        [
+            'id' => 1,
+            'title' => 'Judul Artikel 1',
+            'author' => 'Marqoms',
+            'body' => 'Lorem ipsum dolor sit amet
+consectetur adipisicing elit. Itaque, rerum veniam. Iure, expedita corrupti
+veritatis esse earum maiores cupiditate est illo dolorum consectetur error
+sapiente, temporibus vel. Neque,tempora quos',
+        ],
+        [
+            'id' => 2,
+            'title' => 'Judul Artikel 2',
+            'author' => 'Marqoms',
+            'body' => 'Lorem ipsum dolor, sit amet consectetur
+adipisicing elit. Repellendus quidem, voluptatibus a quisquam quae numquam eum
+soluta porro atque at placeat blanditiis ex harum et ratione aliquid reiciendis
+nostrum cupiditate.',
+        ],
+    ];
+    return view('posts', ['title' => 'blog', 'posts' => $posts]);
 });
 Route::get('/about', function () {
     return view('about', ['title' => 'About']);
