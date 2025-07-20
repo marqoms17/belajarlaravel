16e3a3500d87ec60a0c8bfc32c1dd125828bf7c3
marqoms17 <marqoms17@gmail.com>
2025-07-20 20:21:18 +0700

    add simple post searching

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,19 +1,4 @@
 <x-layout :title="$title">
-    {{-- <article class="py-8 max-w-screen-md">
-        <h2 class="mb-1 text-3xl tracking-tight font-bold to-gray-900">
-            {{ $post['title'] }}
-        </h2>
-        <div class="text-base text-gray-500">
-            <a href="/authors/{{ $post->author->username }}" class="hover:underline">{{ $post->author->name }}</a> | 1
-            Juli 2025
-        </div>
-        <p class="my-4 font-light">
-            {{ $post['body'] }}
-        </p>
-        <a href="/posts" class="font-medium text-blue-500 hover:underline">&laquo; Back to all
-            posts.</a>
-    </article> --}}
-
 
     <main class="pt-8 pb-16 lg:pt-16 lg:pb-24 bg-white dark:bg-gray-900 antialiased">
         <div class="flex justify-between px-4 mx-auto max-w-screen-xl ">
