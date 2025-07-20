0481e0e7723058c25908c56f3cb8e6d08fb7ecf9
marqoms17 <marqoms17@gmail.com>
2025-07-20 19:42:33 +0700

    update ui single post

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,29 +1,4 @@
 <x-layout :title="$title">
-    <!-- Your content -->
-    {{-- @foreach ($posts as $post)
-        <article class="py-8 max-w-screen-md border-b border-gray-300">
-            <a href="/posts/{{ $post['slug'] }}" class="hover:underline">
-                <h2 class="mb-1 text-3xl tracking-tight font-bold to-gray-900">
-                    {{ $post['title'] }}
-                </h2>
-            </a>
-            <div class="text-base text-gray-500">
-                By <a href="/authors/{{ $post->author->username }}"
-                    class="text-gray-900 hover:underline">{{ $post->author->name }}</a>
-                in <a href="/categories/{{ $post->category->slug }}"
-                    class="text-gray-900 hover:underline">{{ $post->category->name }}</a> | 1
-                Juli
-                2025
-            </div>
-            <p class="my-4
-                    font-light">
-                {{ Str::limit($post['body'], 100) }}
-            </p>
-            <a href="/posts/{{ $post['slug'] }}" class="font-medium text-blue-500 hover:underline">Read more
-                &raquo;</a>
-        </article>
-    @endforeach --}}
-
     <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
         <div class="grid gap-8 lg:grid-cols-3 md:grid-cols-2">
             @foreach ($posts as $post)
